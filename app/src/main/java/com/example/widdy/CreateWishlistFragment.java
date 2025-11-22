50b259fcd518437cfcc078d82e9f2711b9c48299
JoudJ <joudjalal5@gmail.com>
2025-11-22 22:15:12 +0300

    Modifications

--- app/src/main/java/com/example/widdy/CreateWishlistFragment.java
+++ app/src/main/java/com/example/widdy/CreateWishlistFragment.java
@@ -22,8 +22,7 @@ import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
